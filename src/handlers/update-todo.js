11b17c4aaf65e7505404a3391f77f1f20ce01dc4
SofiyaYehorova <yehorova66@gmail.com>
2023-04-20 13:47:50 +0300

    init

---
+++ src/handlers/update-todo.js
@@ -0,0 +1,35 @@
+const {ObjectId} = require("mongodb");
+
+const {dbConnection} = require("../mongo.client");
+
+const updateTodo = async (event) => {
+    const {completed} = JSON.parse(event.body);
+    const {todoId} = event.pathParameters;
+
+    const todo = await dbConnection.collection('todos').findOne({_id: new ObjectId(todoId)});
+
+    if (!todo) return {
+        statusCode: 422,
+        body: 'Entity not found',
+    }
+
+    await dbConnection.collection('todos').updateOne(
+        {_id: todo._id},
+        {
+            $set: {
+                updateTodo: new Date().toISOString(),
+                completed
+            }
+        }
+    );
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify('Updated')
+    }
+};
+
+
+module.exports = {
+    handler: updateTodo,
+}
